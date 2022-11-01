2f306041d84efa2eda7ee3660e037e0405c86b77
itschrisagain <chris.achinga@student.moringaschool.com>
2022-11-01 10:13:32 +0300

    completed

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,30 @@
+# require ''
 class Triangle
   # write code here
+  attr_reader :x, :y, :z
+  
+  def initialize(x, y, z)
+    @x = x
+    @y = y
+    @z = z
+  end
+
+  def kind
+    validate_triangle
+    if x == y && y == z
+      :equilateral
+    elsif x == y || y == z || x == z
+      :isosceles
+    else
+      :scalene
+    end
+  end
+
+  def sides_greater_than_zero?
+    [x, y, z].all?(&:positive?)
+  end
+
+  def valid_triangle_inequality?
+    x + y > z && x + z > y && y + z > x
+  end
 end
